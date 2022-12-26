17a448ca6502565dfd2a18b51d5c1f89c62b326e
napSec <113065386+napSec@users.noreply.github.com>
2022-12-26 17:11:03 -0600

    Update top100.py

--- top100.py
+++ top100.py
@@ -1,42 +1,41 @@
-#! python3
-
+#! Python3
 import requests
 
 def get_top_100_addresses(address):
     # Set the API endpoint
     endpoint = "https://blockchain.info/rawaddr/"
 
-    # Send a request to the endpoint to get the list of transactions
+    # Send request to endpoint to retrieve list of transactions
     response = requests.get(endpoint + address)
     data = response.json()
 
-    # Extract transactions from  response
+    # Extract transactions from the response
     transactions = data['txs']
 
-    # Create dictionary to store the addresses / counts
+    # Create dictionary to store the addresses and their counts
     address_counts = {}
 
-    # Iterate through the list of transactions
+    # Iterate list of transactions
     for transaction in transactions:
-        # Get list of inputs for the transaction
+        # Get the list inputs for the transaction
         inputs = transaction['inputs']
         # Iterate through the list of inputs
         for input in inputs:
-            # Get Input address 
+            # Get input address 
             input_address = input['prev_out']['addr']
-            # Increment  count for the input address
+            # Increment input address count
             if input_address in address_counts:
                 address_counts[input_address] += 1
             else:
                 address_counts[input_address] = 1
 
-        # Get outputs for the transaction
+        # Get list of outputs for the transaction
         outputs = transaction['out']
-        # Iterate through the list of outputs
+        # Iterate list of outputs
         for output in outputs:
-            # Get the address of the output
+            # Get address of the output
             output_address = output['addr']
-            # Increment the count for the output address
+            # Increment output address count
             if output_address in address_counts:
                 address_counts[output_address] += 1
             else:
@@ -45,13 +44,15 @@ def get_top_100_addresses(address):
     # Sort the dictionary by values in descending order
     sorted_addresses = sorted(address_counts.items(), key=lambda x: x[1], reverse=True)
 
-    # Return the top 100 addresses
+    # Return top 100 addresses
     return sorted_addresses[:100]
 
-# Run the function with the specified address
-top_100_addresses = get_top_100_addresses("target address")
+# Get address from the user
+address = input("Enter the address: ")
+
+# Test the function with the specified address
+top_100_addresses = get_top_100_addresses(address)
 
 # Print the top 100 addresses
 for address, count in top_100_addresses:
     print(address, count)
-
